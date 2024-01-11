6417b4631686e088b67675f34d68161269e08db9
HKumar6 <153508781+Harishk9697@users.noreply.github.com>
2024-01-11 12:25:24 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,21 +7,28 @@ if [ -d "$folder_to_cleanup" ]
 then
     echo "cleaning up folder $folder_to_cleanup"
     rm -rf "$folder_to_cleanup"
+    mkdir backstopJS
 else
     echo "$folder_to_cleanup does not exist."
+    mkdir backstopJS
 fi
+
+echo "change directory to playwright repo"
+cd backstopJS
+echo "list the files"
+ls
 
 echo "Cloning Git branch..."
 ## Clone the Github repository
-git clone --single-branch --branch main https://github.com/Harishk9697/backstop-test-suite.git /vrt/backstopJS
+#git clone --single-branch --branch main https://github.com/Harishk9697/backstop-test-suite.git /vrt/backstopJS
+aws s3 cp --acl bucket-owner-full-control --recursive s3://tf-rf-scripts-spe-qaqc-bucket/SPT_VRT/aut-vrt-spt/ .
 if [ $? -ne 0 ]; then
     echo "Command Git clone failed"
 fi
 
-echo "change directory to playwright repo"
-cd /vrt/backstopJS
-echo "list the files"
-ls
+backstop init
+cp -r ./Director_Diverse/backstop_data/engine_scripts ./backstop_data/
+
 
 #echo "Install npm"
 ## Install dependencies
@@ -29,21 +36,26 @@ ls
 
 echo "Running reference command..."
 ## RUN backstop reference
-backstop reference --config="backstop.json"
+backstop reference --config="./Director_Diverse/backstop.json"
+#backstop reference --config="backstop.json"
 if [ $? -ne 0 ]; then
     echo "Backstop reference command failed"
 fi
+Sleep 5
 echo "Running test command..."
 ## RUN tests
-#backstop test --config="backstop.json"
+backstop test --config="./Director_Diverse/backstop.json"
 if [ $? -ne 0 ]; then
     echo "Backstop test command failed"
-    aws s3 cp --acl bucket-owner-full-control --recursive /vrt/backstopJS/backstop_data s3://tf-rf-scripts-spe-qaqc-bucket/BackstopJSReport/ --exclude "engine_scripts/*" && echo "Copied report to s3 bucket" || echo "Copying report to s3 bucket failed"
+    Sleep 5
+    aws s3 cp --acl bucket-owner-full-control --recursive ./backstop_data s3://tf-rf-scripts-spe-qaqc-bucket/BackstopJSReport/Director_Diverse --exclude "engine_scripts/*" && echo "Copied report to s3 bucket" || echo "Copying report to s3 bucket failed"
 else
     echo "Backstop test command passed"
-    aws s3 cp --acl bucket-owner-full-control --recursive /vrt/backstopJS/backstop_data s3://tf-rf-scripts-spe-qaqc-bucket/BackstopJSReport/ --exclude "engine_scripts/*" && echo "Copied report to s3 bucket" || echo "Copying report to s3 bucket failed"
+    Sleep 5
+    aws s3 cp --acl bucket-owner-full-control --recursive ./backstop_data s3://tf-rf-scripts-spe-qaqc-bucket/BackstopJSReport/Director_Diverse --exclude "engine_scripts/*" && echo "Copied report to s3 bucket" || echo "Copying report to s3 bucket failed"
 fi
 
+#aws s3 cp --acl bucket-owner-full-control --recursive /vrt/backstopJS/BrandsPortal/backstop_data s3://tf-rf-scripts-spe-qaqc-bucket/Backstop_JS_SPT_report/ --exclude "engine_scripts/*"
 ## Set variable for clone url
 #ARG GITHUB_URL
 #ARG BRANCH_NAME
