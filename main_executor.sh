a8f56119b1262e27ce2d46ce20f8dd7ea4c5a523
Harishk9697 <153508781+Harishk9697@users.noreply.github.com>
2025-02-06 14:01:11 +0530

    Update main_executor.sh

--- main_executor.sh
+++ main_executor.sh
@@ -41,9 +41,10 @@ curl -T "./target/$DEPENDENCY_ZIP" $TEST_PACKAGE_UPLOAD_URL
 echo "Downloading APK/IPA from S3 and uploading to Device Farm..."
 aws s3 cp s3://$S3_BUCKET/$IPA_FILE .
 
-# Upload the APK or IPA to Device Farm
+# Upload the APK or IPA to Device Farm 
 echo "Uploading APK/IPA file to Device Farm..."
-APP_UPLOAD=$(aws devicefarm create-upload --project-arn "$PROJECT_ARN" --name "$IPA_FILE" --type "IOS_APP")
+#APP_UPLOAD=$(aws devicefarm create-upload --project-arn "$PROJECT_ARN" --name "$IPA_FILE" --type "IOS_APP")
+APP_UPLOAD=$(aws devicefarm create-upload --project-arn "$PROJECT_ARN" --name "$IPA_FILE" --type "ANDROID_APP")
 APP_UPLOAD_ARN=$(echo $APP_UPLOAD | jq -r '.upload.arn')
 APP_UPLOAD_URL=$(echo $APP_UPLOAD | jq -r '.upload.url')
 
